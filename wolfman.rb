b1d03fb4702ed730633c4448941f1dd96e5e0137
Matt McCollow <matt@bowser.bowser>
2010-07-28 21:21:29 +0000

    Stubbed out some methods

--- wolfman.rb
+++ wolfman.rb
@@ -3,4 +3,23 @@ require 'rubygems'
 require 'mp3info'
 
 class Wolfman
+  def initialize(time, dir)
+    @time, @dir = time, dir
+  end
+
+  def index
+    #create and index of songs from @dir
+  end
+
+  def create_record
+    #assemble an mp3's filename/path and play time into a DB record
+  end
+
+  def random_song
+    #select a random song from the database
+  end
+
+  def write_playlist(format)
+    #write out the playlist in .pls or .m3u
+  end
 end
