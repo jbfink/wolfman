81d612f86b85d0e1015b579351b938101ef0d38b
Matt McCollow <matt@bowser.bowser>
2010-07-29 20:52:32 +0000

    Added method visibility modifiers

--- wolfman.rb
+++ wolfman.rb
@@ -13,19 +13,21 @@ class Wolfman
     end
   end
 
-  def index
-    #create and index of songs from @dir
-  end
+  public
+    def index
+      #create and index of songs from @dir
+    end
 
-  def create_record
-    #assemble an mp3's filename/path and play time into a DB record
-  end
+    def write_playlist(format)
+      #write out the playlist in .pls or .m3u
+    end
 
-  def random_song
-    #select a random song from the database
-  end
+  private
+    def create_record
+      #assemble an mp3's filename/path and play time into a DB record
+    end
 
-  def write_playlist(format)
-    #write out the playlist in .pls or .m3u
-  end
+    def random_song
+      #select a random song from the database
+    end
 end
