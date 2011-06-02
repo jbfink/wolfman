8ecb0f016efdc5e7871a1f697b703922b63e5f25
Matt McCollow <mmccollow@gmail.com>
2011-06-02 17:17:25 -0400

    Create and populate an SQLite3 DB with track info

--- wolfman.rb
+++ wolfman.rb
@@ -4,20 +4,27 @@ require 'mp3info'
 require 'sqlite3'
 
 class Wolfman
-  def initialize(time, dir)
+  def initialize(time, path)
     #we assume time is given in minutes
-    if File.exists?(dir) && File.directory?(dir)
-      @time, @dir = time, dir
+    if File.exists?(path) && File.directory?(path)
+      @time, @path = time, path
     else
       raise "Supplied directory does not exist or is not a directory"
     end
     @filled = 0
     @tracks = []
+    @db = SQLite3::Database.new("wolfman.db")
+    @db.execute("CREATE TABLE IF NOT EXISTS songs (title varchar(100), length integer)")
   end
 
   public
     def index
-      #create and index of songs from @dir
+      #create an index of songs from @path
+      Dir.foreach(@path) do |filename|
+        Mp3Info.open(filename) do |mp3|
+          @db.execute("INSERT INTO songs VALUES(?, ?)", mp3.tag1.title, mp3.length.ceil())
+        end
+      end
     end
 
     def fill_time
@@ -32,10 +39,6 @@ class Wolfman
     end
 
   private
-    def create_record
-      #assemble an mp3's filename/path and play time into a DB record
-    end
-
     def random_song
       # get new song with length <= (@time - @filled)
       # return song as mp3info object
